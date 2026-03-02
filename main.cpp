1bbb03c2a2f174f8b28d04c44baa8f86f4cd23a5
Sunny Kim <154963758+sskeme@users.noreply.github.com>
2026-03-01 19:23:11 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include "car.h"
+
+int main() {
+  string n;
+  double r;
+  double l;
+  double v;
+
+  // TODO: create an object named car1 from the tank_car class, calling the constructor without parameters
+  // TODO: copy the value of car1's radius to the variable r
+  // TODO: copy the value of car1's length to the variable l
+  // TODO: copy the value of car1's name to the variable n
+  // TODO: calculate the volume of car1 using one of the member functions and store it in the variable v
+
+  cout << "== Information on " << n << " ==" << endl;
+  cout << "  radius: " << r << endl;
+  cout << "  length: " << l << endl;
+  cout << "  volume: " << v << endl;
+
+  // TODO: update car1's radius to 3.4
+  // TODO: update car1's length to 10.1
+  // TODO: update car1's name to "TC1"
+
+  // TODO: copy the value of car1's radius to the variable r
+  // TODO: copy the value of car1's length to the variable l
+  // TODO: copy the value of car1's name to the variable n
+  // TODO: calculate the volume of car1 using one of the member functions and store it in the variable v
+
+  cout << "== Information on " << n << " ==" << endl;
+  cout << "  radius: " << r << endl;
+  cout << "  length: " << l << endl;
+  cout << "  volume: " << v << endl;
+
+  // TODO: create an object named car2 from the tank_car class, calling the constructor with parameters
+  //     : pass the arguments 5.6, 2.1, and "TC2" for radius, length, and name, respectively
+  // TODO: copy the value of car2's radius to the variable r
+  // TODO: copy the value of car2's length to the variable l
+  // TODO: copy the value of car2's name to the variable n
+  // TODO: calculate the volume of car2 using one of the member functions and store it in the variable v
+
+  cout << "== Information on " << n << " ==" << endl;
+  cout << "  radius: " << r << endl;
+  cout << "  length: " << l << endl;
+  cout << "  volume: " << v << endl;
+
+  return 0;
+}
